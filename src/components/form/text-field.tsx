32178fcb6fd761a90b1247471d3230fbfe9f928b
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-10-16 14:16:13 +0700

    change shadcn components

--- src/components/form/text-field.tsx
+++ src/components/form/text-field.tsx
@@ -2,25 +2,33 @@ import { InputHTMLAttributes } from "react";
 
 import { cn } from "@/lib/utils";
 
-import { Input } from "../ui/input";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "../ui/input-group";
 import { useFieldContext } from ".";
 
 interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
+  addonLeft?: React.ReactNode;
+  addonRight?: React.ReactNode;
   className?: string;
 }
 
 export default function TextField({
   placeholder,
+  addonLeft,
+  addonRight,
   className,
   ...inputProps
 }: TextFieldProps) {
   const field = useFieldContext<string>();
-  // const errors = useStore(field.store, (state) => state.meta.errors);
 
   return (
-    <div className="flex flex-col">
-      <Input
+    <InputGroup>
+      {addonLeft && <InputGroupAddon>{addonLeft}</InputGroupAddon>}
+      <InputGroupInput
         id={field.name}
         type="text"
         value={field.state.value}
@@ -30,14 +38,22 @@ export default function TextField({
         onChange={(e) => field.handleChange(e.target.value)}
         {...inputProps}
       />
-
-      {/* {field.state.meta.isTouched &&
-        errors.map((error: string) => (
-          <div key={error} className="flex items-center gap-x-1">
-            <AlertCircle size="15" className="text-destructive" />
-            <p className="text-xs text-destructive">{error}</p>
-          </div>
-        ))} */}
-    </div>
+      {addonRight && (
+        <InputGroupAddon align="inline-end">{addonRight}</InputGroupAddon>
+      )}
+    </InputGroup>
   );
+}
+
+{
+  /* <Input
+  id={field.name}
+  type="text"
+  value={field.state.value}
+  placeholder={placeholder}
+  autoComplete="off"
+  className={cn("", className)}
+  onChange={(e) => field.handleChange(e.target.value)}
+  {...inputProps}
+/> */
 }
