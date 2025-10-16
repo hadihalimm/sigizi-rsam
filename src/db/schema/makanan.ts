6b6f80742e9a77ff31a28994c0b99db38018b147
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-10-16 14:16:35 +0700

    add unique constraint

--- src/db/schema/makanan.ts
+++ src/db/schema/makanan.ts
@@ -1,16 +1,18 @@
+import { relations } from "drizzle-orm";
 import {
   doublePrecision,
   integer,
   pgTable,
   serial,
   text,
+  unique,
 } from "drizzle-orm/pg-core";
 
 import { bahanMakanan } from "./bahan-makanan";
 
 export const makananType = pgTable("makanan_type", {
   id: serial("id").primaryKey(),
-  code: text("code").notNull(),
+  code: text("code").notNull().unique(),
   name: text("name").notNull(),
 });
 
@@ -22,13 +24,17 @@ export const makanan = pgTable("makanan", {
     .references(() => makananType.id, { onDelete: "no action" }),
 });
 
-export const makananResepDetail = pgTable("makanan_resep_detail", {
-  id: serial("id").primaryKey(),
-  makananId: integer("makanan_id")
-    .notNull()
-    .references(() => makanan.id, { onDelete: "cascade" }),
-  bahanMakananId: integer("bahan_makanan_id")
-    .notNull()
-    .references(() => bahanMakanan.id, { onDelete: "cascade" }),
-  quantity: doublePrecision("quantity").notNull(),
-});
+export const makananResepDetail = pgTable(
+  "makanan_resep_detail",
+  {
+    id: serial("id").primaryKey(),
+    makananId: integer("makanan_id")
+      .notNull()
+      .references(() => makanan.id, { onDelete: "cascade" }),
+    bahanMakananId: integer("bahan_makanan_id")
+      .notNull()
+      .references(() => bahanMakanan.id, { onDelete: "cascade" }),
+    quantity: doublePrecision("quantity").notNull(),
+  },
+  (t) => [unique().on(t.makananId, t.bahanMakananId)]
+);
