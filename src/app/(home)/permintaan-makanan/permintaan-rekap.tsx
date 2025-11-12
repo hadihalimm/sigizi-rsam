d955ee53d8280d855d4471e635fa798d5967261e
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-11-12 09:50:57 +0700

    add jumlah info

--- src/app/(home)/permintaan-makanan/permintaan-rekap.tsx
+++ src/app/(home)/permintaan-makanan/permintaan-rekap.tsx
@@ -30,17 +30,35 @@ const PermintaanRekap = () => {
     orpc.makananType.getAll.queryOptions()
   );
 
+  let totalCount = 0;
+  let vipCount = 0;
+  let mcTssCount = 0;
   const matrix: Record<string, Record<string, number>> = {};
+  const makananTypeTotals: Record<string, number> = {};
   treatmentClassList.forEach((tc) => {
     matrix[tc.code] = {};
     makananTypeList.forEach((mt) => {
       matrix[tc.code][mt.code] = 0;
+      makananTypeTotals[mt.code] = 0;
     });
   });
   data.forEach((item) => {
     const tcCode = item.treatmentClass.code;
     const mtCode = item.makananType.code;
     matrix[tcCode][mtCode] += 1;
+    makananTypeTotals[mtCode] += 1;
+
+    if (
+      mtCode.toLowerCase().includes("ml") ||
+      mtCode.toLowerCase().includes("tss")
+    ) {
+      mcTssCount++;
+    }
+    if (tcCode.toLowerCase().includes("vip")) {
+      vipCount++;
+    } else {
+      totalCount++;
+    }
   });
 
   return (
@@ -74,8 +92,34 @@ const PermintaanRekap = () => {
               ))}
             </TableRow>
           ))}
+          <TableRow>
+            <TableCell className="font-semibold">Total</TableCell>
+            {makananTypeList.map((mt) => (
+              <TableCell key={mt.code}>
+                {makananTypeTotals[mt.code] || ""}
+              </TableCell>
+            ))}
+          </TableRow>
         </TableBody>
       </Table>
+
+      <div className="flex flex-col gap-2 text-sm">
+        <div className="flex">
+          <p className="w-1/2">Jumlah VIP : </p>
+          <p className="w-4">:</p>
+          <p className="">{vipCount}</p>
+        </div>
+        <div className="flex">
+          <p className="w-1/2">Jumlah I + II + III : </p>
+          <p className="w-4">:</p>
+          <p className="">{totalCount}</p>
+        </div>
+        <div className="flex">
+          <p className="w-1/2">Jumlah ML + TSS : </p>
+          <p className="w-4">:</p>
+          <p className="">{mcTssCount}</p>
+        </div>
+      </div>
     </div>
   );
 };
