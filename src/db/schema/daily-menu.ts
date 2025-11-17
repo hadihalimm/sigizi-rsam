5094e88eea63a968539b86f5f15be845766f688d
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-11-17 11:06:52 +0700

    change date mode to object

--- src/db/schema/daily-menu.ts
+++ src/db/schema/daily-menu.ts
@@ -1,4 +1,11 @@
-import { date, integer, pgTable, serial, timestamp } from "drizzle-orm/pg-core";
+import {
+  date,
+  integer,
+  pgTable,
+  serial,
+  timestamp,
+  unique,
+} from "drizzle-orm/pg-core";
 
 import { makanan } from "./makanan";
 import { menu } from "./menu";
@@ -6,7 +13,7 @@ import { snack } from "./snack";
 
 export const dailyMenu = pgTable("daily_menu", {
   id: serial("id").primaryKey(),
-  day: date("day").notNull(),
+  day: date("day", { mode: "date" }).notNull(),
   menuId: integer("menu_id").references(() => menu.id, {
     onDelete: "set null",
   }),
@@ -19,22 +26,34 @@ export const dailyMenu = pgTable("daily_menu", {
     .$onUpdate(() => new Date()),
 });
 
-export const dailyMenuMakananDetail = pgTable("daily_menu_makanan_detail", {
-  id: serial("id").primaryKey(),
-  dailyMenuId: integer("daily_menu_id")
-    .notNull()
-    .references(() => dailyMenu.id, { onDelete: "cascade" }),
-  makananId: integer("makanan_id")
-    .notNull()
-    .references(() => makanan.id, { onDelete: "cascade" }),
-});
+export const dailyMenuMakananDetail = pgTable(
+  "daily_menu_makanan_detail",
+  {
+    id: serial("id").primaryKey(),
+    dailyMenuId: integer("daily_menu_id")
+      .notNull()
+      .references(() => dailyMenu.id, { onDelete: "cascade" }),
+    makananId: integer("makanan_id")
+      .notNull()
+      .references(() => makanan.id, { onDelete: "cascade" }),
+  },
+  (table) => [
+    unique("unique_daily_menu_makanan").on(table.dailyMenuId, table.makananId),
+  ]
+);
 
-export const dailyMenuSnackDetail = pgTable("daily_menu_snack_detail", {
-  id: serial("id").primaryKey(),
-  dailyMenuId: integer("daily_menu_id")
-    .notNull()
-    .references(() => dailyMenu.id, { onDelete: "cascade" }),
-  snackId: integer("snack_id")
-    .notNull()
-    .references(() => snack.id, { onDelete: "cascade" }),
-});
+export const dailyMenuSnackDetail = pgTable(
+  "daily_menu_snack_detail",
+  {
+    id: serial("id").primaryKey(),
+    dailyMenuId: integer("daily_menu_id")
+      .notNull()
+      .references(() => dailyMenu.id, { onDelete: "cascade" }),
+    snackId: integer("snack_id")
+      .notNull()
+      .references(() => snack.id, { onDelete: "cascade" }),
+  },
+  (table) => [
+    unique("unique_daily_menu_snack").on(table.dailyMenuId, table.snackId),
+  ]
+);
