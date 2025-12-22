66e58478859966db3cc463e6fc37661adf92417b
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-12-22 08:28:54 +0700

    add generate htmlShell helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,29 @@ export function isFieldInvalid(meta: AnyFieldMeta) {
 
 export const capitalizeFirst = (s: string) =>
   s.charAt(0).toUpperCase() + s.slice(1);
+
+export function htmlShell(body: string) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100..900&display=swap"
+        rel="stylesheet"
+      />
+      <title>Etiket Diet</title>
+      <script src="https://cdn.tailwindcss.com"></script>
+      <style>
+        body {
+          width: 384px
+          margin: 0;
+        }
+      </style>
+    </head>
+    <body>
+      ${body}
+    </body>
+  </html>`;
+}
